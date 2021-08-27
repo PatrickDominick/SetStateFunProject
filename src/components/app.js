7aeb91a3ccb579aaa3c110139c6d3cc93e7a9b95
PatrickDominick <pdneary@gmail.com>
2021-08-27 12:09:04 -0500

    Align Me and Hide Me done. Added some styles and a mixin which was my first solo mixin done!

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import AlignMe from "./align-me";
+import HideMe from "./hide-me";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <AlignMe />
+        <HideMe />
       </div>
     );
   }
---
+++ src/components/hide-me.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class HideMe extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        }
+        this.hide = this.hide.bind(this)
+        
+    }
+
+    hide() {
+        const textToHide = document.getElementById("text-to-hide");
+        if (textToHide.style.display === "none") {
+            textToHide.style.display = "block";
+        } else {
+            textToHide.style.display = "none";
+        }
+    }
+
+    render() {
+        return (
+            <div className="hide-me">
+                <h1 id = "text-to-hide">Hide Me!</h1>
+                <div className="hide-me-btn">
+                    <button onClick = {this.hide}>Hide</button>
+                </div>
+            </div>
+        )
+    }
+}
+   
+
+//put html on page
+//add onclick handlers
